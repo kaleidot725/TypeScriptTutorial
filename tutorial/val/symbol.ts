2c01848cfdd31463683421fa323e0f5bda1fee9e
kaleidot725 <hakodate.katsuragawa.yusuke@gmail.com>
2024-12-13 22:48:09 +0900

    feat: update

---
+++ tutorial/val/symbol.ts
@@ -0,0 +1,4 @@
+const s1 = Symbol("foo");
+const s2 = Symbol("foo");
+console.log(s1 === s1);
+console.log(s1 === s2);
---
+++ tutorial/val/undefined.ts
@@ -0,0 +1,13 @@
+let name;
+console.log(name);
+
+function func() {}
+console.log(func());
+
+const obj = {};
+console.log(obj.name);
+
+const arr = [];
+console.log(arr[1]);
+
+const x: undefined = undefined;
